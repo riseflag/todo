96ce8bad3114fd7b6a809cc43d547428b1733b69
decaweb <kevindhoury@gmail.com>
2025-03-26 11:16:34 +0100

    Ajout de la fonctionnalité d'ajout de todo

--- src/main.js
+++ src/main.js
@@ -1,2 +1,51 @@
 import './style.css'
 
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const value = input.value;
+    input.value = '';
+    addTodo(value);
+    displayTodo();
+})
+
+const todos = [
+    {
+        text: 'Je suis une todo',
+        done: false
+    },
+    {
+        text: 'Faire du JavaScript',
+        done: true
+    },
+]
+
+const displayTodo = () => {
+    const todosNode = todos.map((todo, index) => {
+        return createTodoElement(todo, index);
+    })
+    ul.innerHTML = '';
+    ul.append(...todosNode);
+}
+
+const createTodoElement = (todo, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <span class="todo ${ todo.done ? 'done' : ''}"></span>
+    <p>${ todo.text }</p>
+    <button>Supprimer</button>
+    `;
+    return li;
+}
+
+const addTodo = (text) => {
+    todos.push({
+        text,
+        done: false
+    });
+}
+
+displayTodo();
