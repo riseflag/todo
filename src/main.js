ce02ad9afc820466eec64b5eaa573ae245fc2c62
decaweb <kevindhoury@gmail.com>
2025-03-26 16:57:14 +0100

    Ajout fonctionnalité modification todo

--- src/main.js
+++ src/main.js
@@ -14,17 +14,23 @@ form.addEventListener('submit', (event) => {
 const todos = [
     {
         text: 'Je suis une todo',
-        done: false
+        done: false,
+        editMode: false
     },
     {
         text: 'Faire du JavaScript',
-        done: true
+        done: true,
+        editMode: false
     },
 ]
 
 const displayTodo = () => {
     const todosNode = todos.map((todo, index) => {
-        return createTodoElement(todo, index);
+        if (todo.editMode) {
+            return createTodoEditElement(todo, index);
+        }else{
+            return createTodoElement(todo, index);
+        }
     })
     ul.innerHTML = '';
     ul.append(...todosNode);
@@ -35,10 +41,16 @@ const createTodoElement = (todo, index) => {
     const li = document.createElement('li');
     const buttonDelete = document.createElement('button');
     buttonDelete.innerHTML = "Supprimer";
+    const buttonEdit = document.createElement('button');
+    buttonEdit.innerHTML = "Modifier";
     buttonDelete.addEventListener('click', (event) => {
         event.stopPropagation();
         deleteTodo(index);
     });
+    buttonEdit.addEventListener('click', event => {
+        event.stopPropagation();
+        toggleEditMode(index);
+    })
     li.innerHTML = `
     <span class="todo ${ todo.done ? 'done' : ''}"></span>
     <p>${ todo.text }</p>
@@ -46,7 +58,28 @@ const createTodoElement = (todo, index) => {
     li.addEventListener('click', (event) => {
         toggleTodo(index);
     });
+    li.appendChild(buttonEdit);
     li.appendChild(buttonDelete);
+    return li;
+}
+
+const createTodoEditElement = (todo, index) => {
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = todo.text;
+    const buttonSave = document.createElement('button');
+    buttonSave.innerHTML = "Enregistrer";
+    const buttonCancel = document.createElement('button');
+    buttonCancel.innerHTML = "Annuler";
+    buttonCancel.addEventListener('click', event => {
+        event.stopPropagation();
+        toggleEditMode(index);
+    })
+    buttonSave.addEventListener('click', event => {
+        editTodo(index, input);
+    })
+    li.append(input, buttonCancel, buttonSave);
     return li;
 }
 
@@ -65,6 +98,18 @@ const deleteTodo = (index) => {
 
 const toggleTodo = index => {
     todos[index].done = !todos[index].done;
+    displayTodo();
+};
+
+const toggleEditMode = index => {
+    todos[index].editMode = !todos[index].editMode;
+    displayTodo();
+}
+
+const editTodo = (index, input) => {
+    const value = input.value;
+    todos[index].text = value;
+    todos[index].editMode = false;
     displayTodo();
 };
 
